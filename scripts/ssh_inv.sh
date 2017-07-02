6a13bf5e9d897c5ae9351dd0a4d529641c59ff7e
txurtxil <jubadomi@hotmail.com>
2017-07-02 07:42:37 +0200

    scripts/ssh_inv.sh Script conexion ssh inverso

--- scripts/ssh_inv.sh
+++ scripts/ssh_inv.sh
@@ -5,40 +5,32 @@ set -e
 # your PC via GPRS / 3G  without public ip
 # manual: http://surfero.blogspot.com.es/2017/05/conexion-por-red-de-datos-movil-2g-g.html
 
-REMOTE_ADDRESS=$(whiptail --inputbox "IP publica remota" 8 78 Blue --title "Conexion ssh inverso" 3>&1 1>&2 2>&3)
-REMOTE_PORT=$(whiptail --inputbox "Introduce el puerto de tu srv ssh " 8 78 Blue --title "Conexion ssh inverso" 3>&1 1>&2 2>&3)
-PASSWORD=$(whiptail --inputbox "Introduce la PASSWORD de tu srv ssh " 8 78 Blue --title "Conexion ssh inverso" 3>&1 1>&2 2>&3)
+FWD_PORT=1234
+WAIT_SECONDS=60
+REMOTE_ADDRESS=$1
+REMOTE_PORT=$2
+PASSWORD=$3
 
-       	FWD_PORT=1234
-	WAIT_SECONDS=60
-        # si el numero de argumentos no es igual a 3 ejecutamos esta ayuda y salimos:
-#	if [ "$#" != "2" ]; then
-#	 echo "Maintain a reverse ssh connection, forwarding port $FWD_PORT on the remote machine."
-#	 echo "If the connection fails or is dropped, wait $WAIT_SECONDS seconds and retry."
-#	 echo "Usage: $(basename $0) [remote_user@]remote_server port password"
-#	 echo "Then, on the remote ssh server: ssh $USER@localhost -p $FWD_PORT"
-#         exit
-#	fi
+# By default the tunnel never seems to timeout. This is bad because if the connection to the
+# server has been established once, then the server disconnects (link down, or maybe it's a laptop
+# that doesn't run 24/7), it could never be re-established. So it's important to set a timeout for
+# the tunnel. Note that keepalive is handled transparently by ssh; it does not mean any payload data
+# has to be sent through the tunnel at these intervals.
 
-	REMOTE_ADDRESS=$1
-	REMOTE_PORT=$2
-	PASSWORD=$3
-
-	# By default the tunnel never seems to timeout. This is bad because if the connection to the
-	# server has been established once, then the server disconnects (link down, or maybe it's a laptop
-	# that doesn't run 24/7), it could never be re-established. So it's important to set a timeout for
-	# the tunnel. Note that keepalive is handled transparently by ssh; it does not mean any payload data
-	# has to be sent through the tunnel at these intervals.
-	SERVER_ALIVE_INTERVAL=60
-	SERVER_ALIVE_COUNT_MAX=2
+SERVER_ALIVE_INTERVAL=60
+SERVER_ALIVE_COUNT_MAX=2
 	
-	while true; do
-	 sshpass -p "$PASSWORD" \
-	 ssh -o ServerAliveInterval=$SERVER_ALIVE_INTERVAL \
-	 -o ServerAliveCountMax=$SERVER_ALIVE_COUNT_MAX \
-	 -o StrictHostKeyChecking=no \
-	 -o UserKnownHostsFile=/dev/null \
-	 -o ConnectTimeout=15 \
-	 -N -R $FWD_PORT:localhost:22 $REMOTE_ADDRESS -p $REMOTE_PORT
-	 sleep $WAIT_SECONDS
-	done
+while true; do
+ REMOTE_ADDRESS=$(whiptail --inputbox "IP publica remota" 8 78 --title "Conexion ssh inverso" 3>&1 1>&2 2>&3)
+ REMOTE_PORT=$(whiptail --inputbox "Introduce el puerto de tu srv ssh " 8 78 --title "Conexion ssh inverso" 3>&1 1>&2 2>&3)
+ PASSWORD=$(whiptail --inputbox "Introduce la PASSWORD de tu srv ssh " 8 78 --title "Conexion ssh inverso" 3>&1 1>&2 2>&3)	 
+  sshpass -p "$PASSWORD" \
+  ssh -o ServerAliveInterval=$SERVER_ALIVE_INTERVAL \
+      -o ServerAliveCountMax=$SERVER_ALIVE_COUNT_MAX \
+      -o StrictHostKeyChecking=no \
+      -o UserKnownHostsFile=/dev/null \
+      -o ConnectTimeout=15 \
+      -N -R $FWD_PORT:localhost:22 $REMOTE_ADDRESS -p $REMOTE_PORT
+      sleep $WAIT_SECONDS
+done
+
